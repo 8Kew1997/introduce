e79f986759dd6210c2aa147ee2cc31856c441b55
nisa-skntc-git69 <68219010003@skntc.ac.th>
2026-08-17 10:54:57 +0700

    nisa

---
+++ 03_introduce.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("My name is Nisa Phuphakdee\n");
+    printf("My student ID is 68219010003\n");
+    printf("My birthday is 20-09-2552\n");
+    printf("My favorite color is red\n");
+    
+
+    return 0;
+}
