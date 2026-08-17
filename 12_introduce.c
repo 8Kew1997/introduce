91f15ae84a73e6e7cdf4b4123db12506328d2f1f
first commit <68219010013@skntc.ac.th>
2026-08-17 12:05:33 +0700

    byeeee

---
+++ 12_introduce.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+    printf("My name is Tawatchai\n");
+    printf("I like the color blue.\n");
+    printf("I love eating it.\n");
+    printf("16 years old\n");
+    return 0;
+}
