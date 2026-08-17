25ce54ee2c9b0c967f0f05ce80890c003ee220b2
68219010005-jpg <68219010005@skntc.ac.th>
2026-08-17 11:05:08 +0700

    pornthip

---
+++ 005 pornthip.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("My name is pornthip korncharoen\n");
+    printf("My student ID is 68219010005\n");
+    printf("My birthday is 2010-04-22\n");
+    printf("My favorite color is pink\n");
+    printf("My favorite food is nooddles\n");
+
+    return 0;
+}
