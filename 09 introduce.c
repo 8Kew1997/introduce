860541d965e7d1eb1552919fff87a6827bf6dc20
Ailada-skntc-git69 <f22033268@gmail.com>
2026-08-17 10:56:04 +0700

    Add files via upload

---
+++ 09 introduce.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("My name is Ailada Phaprasert\n");
+    printf("My student ID is 68219010009\n");
+    printf("My birthday is 19/01/2553\n");
+    printf("My favorite color is violet\n");
+    
+    return 0;
+}
