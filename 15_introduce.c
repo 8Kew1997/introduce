6b3805a6ccc450fdc56a17b80a19a9787b69b516
OatMealXXII <oatbangping2@gmail.com>
2026-08-17 10:17:27 +0700

    Commit by OatMealXXII

---
+++ 15_introduce.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("My name is Phongthep Chotikansahakul\n");
+    printf("My student ID is 68219010016\n");
+    printf("My birthday is 2009-05-22\n");
+    printf("My favorite color is blue\n");
+    printf("My favorite food is ramen\n");
+
+    return 0;
+}
