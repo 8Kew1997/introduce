fbbc07d38fede4bc18c17f7aa1c22f56b7f14673
First commit code <68219010021@skntc.ac.th>
2026-08-17 10:24:41 +0700

    Bye bye

---
+++ 21_introduce.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    printf("name: wittawat thongngam\n");
+    printf("student ID: 68219010021\n");
+    printf("birthday: 2010-01-06\n");
+    printf("age: 16\n");
+    return 0;
+}
