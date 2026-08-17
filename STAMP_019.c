273fada8ec82ad69451e9d2fdbc6d1f2b0b12b97
STAMP <68219010019@skntc.ac.th>
2026-08-17 10:37:06 +0700

    I am Batman

---
+++ STAMP_019.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void main() {
+printf("Hello! my name Stamp \n");
+printf("My name is Pichayut \n");
+printf("My student ID is 68219010019\n");
+printf("My birthday is 2008-11-27\n");
+printf("My favorite color is Ocean Blue \n");
+printf("My favorite food is Fried rice\n");
+
+}
