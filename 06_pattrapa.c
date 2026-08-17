e3df8ebcdd56eecf1d061620ee80fcee6f115ff0
pattrapa-skntc-git69 <68219010006@skntc.ac.th>
2026-08-17 10:49:15 +0700

    pattrapa

---
+++ 06_pattrapa.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("My name is Pattrapa Aimkrathok\n");
+    printf("My student ID is 68219010007\n");
+    printf("My birthday is 2009-09-13\n");
+    printf("My favorite color is blue\n");
+    printf("My favorite food is nooddles\n");
+
+    return 0;
+}
